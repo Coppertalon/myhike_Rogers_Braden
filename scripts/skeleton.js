564b06fd2e065172b1d4f418aff2c50cf48b2c57
Coppertalon <brogers42@my.bcit.ca>
2022-11-01 12:10:01 -0700

    added lecture 09

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,8 +2,8 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
+function loadSkeleton() {
+    console.log($('#getNavbarPlaceholder').load('./text/nav_lab.html'));
+    console.log($('#getFooterPlaceholder').load('./text/footer_lab.html'));
 }
 loadSkeleton();  //invoke the function
